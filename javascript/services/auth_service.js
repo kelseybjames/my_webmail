f0ce1dcf64284bb734b50393b852f7fd83199a62
Deepa Kackar <dkackar99@yahoo.com>
2016-04-08 08:58:13 -0700

    Added some angular code

---
+++ javascript/services/auth_service.js
@@ -0,0 +1,42 @@
+mailerApp.factory('authService', 'window', 'env',[function(window,env){
+
+  var obj = {};
+ 
+  obj.handleClientLoad = function() {
+    gapi.client.setApiKey(env.apiKey);
+    window.setTimeout(obj.checkAuth, 1);
+  }
+
+  obj.checkAuth = function() {
+    gapi.auth.authorize({
+      client_id: env.clientId,
+      scope: env.scopes,
+      immediate: true
+    }, obj.handleAuthResult);
+  }
+
+  obj.handleAuthClick =  function() {
+    gapi.auth.authorize({
+      client_id: env.clientId,
+      scope: env.scopes,
+      immediate: false
+    }, obj.handleAuthResult);
+    return false;
+  }
+
+  obj.handleAuthResult =  function(authResult) {
+    if(authResult && !authResult.error) {
+      getEmailService.loadGmailApi();
+      $('#compose-button').removeClass("hidden");
+      $('#authorize-button').remove();
+      $('.table-inbox').removeClass("hidden");
+    } else {
+      $('#authorize-button').removeClass("hidden");
+      $('#authorize-button').on('click', function(){
+        obj.handleAuthClick();
+      });
+    }
+  }
+
+  return obj;
+}  

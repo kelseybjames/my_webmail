6905ed46b32759074b1a13ee896eddd0ccc83efc
Deepa Kackar <dkackar99@yahoo.com>
2016-04-08 09:49:11 -0700

    removed secret stuff

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,5 @@
 var MailerApp = angular.module('MailerApp', ['ui.router', 'ui.bootstrap']);
 
-MailerApp.constant('env', {
- clientId: '586929170645-1882ven187ul03hisl6jhb4isrstmt83.apps.googleusercontent.com',
-  apiKey: 'AIzaSyD3-p9DiZtNjptHJrjiPNt78ozi1aoNVvY',
-  scopes:  'https://www.googleapis.com/auth/gmail.readonly '+
-            'https://www.googleapis.com/auth/gmail.send'
-});
-
 MailerApp.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider

f3d10799038c9e2308f764c108bca8089548dc1a
Deepa Kackar <dkackar99@yahoo.com>
2016-04-08 16:20:11 -0700

    send service

---
+++ javascript/services/send_service.js
@@ -0,0 +1,42 @@
+MailerApp.factory('sendService',['$window','env', function($window,env){
+
+  var obj = {}; 
+
+  obj.sendEmail = function(to,subject,body){
+    obj.sendMessage(
+      {
+        'To': to,
+        'Subject': subject
+      },
+      body,
+      composeTidy
+    );
+    return false;
+  }
+
+  obj.sendMessage = function(headers_obj, message, callback) {
+    var email = '';
+
+    for(var header in headers_obj) {
+      email += header += ": "+headers_obj[header]+"\r\n";
+
+      email += "\r\n" + message;
+
+      var sendRequest = $window.gapi.client.gmail.users.messages.send({
+        'userId': 'me',
+        'resource': {
+           'raw': $window.btoa(email).replace(/\+/g, '-').replace(/\//g, '_')
+         }
+      });
+    }
+
+    return sendRequest.execute(callback);
+  }
+
+  
+  obj.composeTidy = function() {
+
+  }
+  
+  return obj;
+}]);
